4383a858babb0c75ad0ef9ff719f45e679cfcf64
Carlaav <92107798+Carlaav@users.noreply.github.com>
2022-04-15 14:40:30 +0200

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,1 +1,14 @@
+import socket
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+mensaje = str(input("Introduce tu mensaje: "))
+
+s.connect((socket.gethostname(), 1234))
+
+s.send((mensaje).encode())
+
+print("Mensaje enviado")
+
+recibido = s.recv(1024).decode()
+print("Mensaje recibido del servidor: " + recibido)
